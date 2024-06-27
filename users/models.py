3d8cbcb940b45077fc4efe9184772ac14b9fc991
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-06-27 16:44:16 +0300

    Correction:     registration     logging

--- users/models.py
+++ users/models.py
@@ -7,9 +7,10 @@ class CustomUser(AbstractUser):
     policy = models.BooleanField(default=False)
     wantInfo = models.BooleanField(default=False)
     wholesale = models.BooleanField(default=False)
+    username = models.CharField(max_length=150, default='default_username')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = ['last_name', 'first_name', 'policy', 'wantInfo', 'wholesale']
 
     def __str__(self):
         return self.email
--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,7 @@ from .models import CustomUser
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -9,7 +10,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'policy', 'wantInfo', 'wholesale']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'policy', 'wantInfo', 'wholesale', 'password']
 
 
 User = get_user_model()
@@ -17,40 +18,21 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['email', 'first_name', 'last_name', 'password', 'policy', 'wantInfo', 'wholesale']
+        model = CustomUser
+        fields = ['id', 'email', 'password', 'first_name', 'last_name', 'policy', 'wantInfo', 'wholesale']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            password=validated_data['password'],
-            policy=validated_data['policy'],
-            wantInfo=validated_data['wantInfo'],
-            wholesale=validated_data['wholesale']
-        )
+        validated_data['username'] = validated_data['email']
+        user = CustomUser.objects.create_user(**validated_data)
         return user
 
 
-class LoginSerializer(serializers.Serializer):
+class LoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField()
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
-    def validate(self, attrs):
-        email = attrs.get('email')
-        password = attrs.get('password')
-
-        if email and password:
-            user = authenticate(email=email, password=password)
-
-            if user:
-                return {
-                    'email': user.email,
-                    'tokens': user.tokens()
-                }
-            else:
-                raise serializers.ValidationError("Unable to log in with provided credentials.")
-        else:
-            raise serializers.ValidationError("Must include 'email' and 'password'.")
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
