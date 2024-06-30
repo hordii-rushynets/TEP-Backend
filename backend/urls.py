3b42317d5f745555b31fc5702856945bb62beb30
hordii.rushynets@internet-techonlogy-hub.com <hordii.rushynets@internet-techonlogy-hub.com>
2024-06-30 14:53:49 +0300

    ADD migration

--- backend/urls.py
+++ backend/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from .settings import MEDIA_ROOT, MEDIA_URL
+from django.conf import settings
 
 
 urlpatterns = [
