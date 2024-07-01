08b6b5a07bbe7dc548cc959840d9766ff96b7f33
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-07-01 13:19:47 +0300

    Added url for get user data

--- users/urls.py
+++ users/urls.py
@@ -1,28 +1,24 @@
 from django.urls import path
 from .views import (
-    UserRegistrationAPIView,
-    OTPVerificationAPIView,
-    PasswordResetRequestAPIView,
-    PasswordResetConfirmAPIView,
-    UserDeleteAPIView,
-    UserProfileUpdateAPIView,
-    UserLoginAPIView,
-    NewOTPPasswordAPIView
+    UserRegistrationAPIView, OTPVerificationAPIView,
+    PasswordResetRequestAPIView, PasswordResetConfirmAPIView,
+    UserDeleteAPIView, UserProfileUpdateAPIView,
+    UserLoginAPIView, NewOTPPasswordAPIView,
+    GetUserDataViewSet
 )
 
 
 urlpatterns = [
     path('register/', UserRegistrationAPIView.as_view(), name='user-register'),
     path('verify-otp/', OTPVerificationAPIView.as_view(), name='verify-otp'),
+    path('login/', UserLoginAPIView.as_view(), name='user-login'),
 
     path('password/reset/', PasswordResetRequestAPIView.as_view(), name='password-reset-request'),
     path('password/reset/confirm/', PasswordResetConfirmAPIView.as_view(), name='password-reset-confirm'),
 
-    path('delete/', UserDeleteAPIView.as_view(), name='user-delete'),
-
+    path('profile/delete/', UserDeleteAPIView.as_view(), name='user-delete'),
     path('profile/update/', UserProfileUpdateAPIView.as_view(), name='user-profile-update'),
+    path('profile/get/', GetUserDataViewSet.as_view(), name='profile-get'),
 
-    path('login/', UserLoginAPIView.as_view(), name='user-login'),
-
-    path('new_otp/', NewOTPPasswordAPIView.as_view(), name='new-otp')
+    path('new_otp/', NewOTPPasswordAPIView.as_view(), name='new-otp'),
 ]
