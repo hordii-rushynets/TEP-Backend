8ebba0649737e00df963ffca5aa2c3967173283e
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-07-03 14:22:18 +0300

    Correcting the url

--- store/urls.py
+++ store/urls.py
@@ -8,33 +8,15 @@ from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
-router.register(r'products', ProductViewSet)
-
+router.register(r'categories', CategoryViewSet, basename='categories')
+router.register(r'products', ProductViewSet, basename='products')
+router.register(r'sizes', SizeViewSet, basename='sizes')
+router.register(r'colors', ColorViewSet, basename='colors')
+router.register(r'materials', MaterialViewSet, basename='materials')
+router.register(r'variants', ProductVariantViewSet, basename='variants')
+router.register(r'variant-info', ProductVariantInfoViewSet, basename='variant-info')
 
 urlpatterns = [
     path('', include(router.urls)),
-]
-
-"""path('categories/', CategoryViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('categories/<slug:slug>/', CategoryViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-
-    path('products/', ProductViewSet.as_view({'get': 'list'})),
     path('products/import/', ProductsImport.as_view()),
-    path('products/<slug:slug>/', ProductViewSet.as_view({'get': 'retrieve'})),
-    path('products/search/', ProductSearchViewSet.as_view({'get': 'list'})),
-
-    path('sizes/', SizeViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('sizes/<slug:slug>/', SizeViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-
-    path('colors/', ColorViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('colors/<slug:slug>/', ColorViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-
-    path('materials/', MaterialViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('materials/<slug:slug>/', MaterialViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-
-    path('variants/', ProductVariantViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('variants/<slug:slug>/', ProductVariantViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-
-    path('variant-info/', ProductVariantInfoViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('variant-info/<slug:slug>/', ProductVariantInfoViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),"""
-
+]
