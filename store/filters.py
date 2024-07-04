62aa00d8bdfd28fe0364412a0789af6367a0fd03
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-07-04 11:42:29 +0300

    Added new fields in Product Filter

--- store/filters.py
+++ store/filters.py
@@ -5,16 +5,28 @@ from .models import Product, Category
 class ProductFilter(django_filters.FilterSet):
     slug = django_filters.CharFilter(field_name='slug', lookup_expr='icontains')
     title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
-    category = django_filters.CharFilter(field_name='category__title', lookup_expr='icontains')
+
     price_min = django_filters.NumberFilter(field_name='productvariant__default_price', lookup_expr='gte')
     price_max = django_filters.NumberFilter(field_name='productvariant__default_price', lookup_expr='lte')
     size = django_filters.CharFilter(field_name='productvariant__sizes__title', lookup_expr='icontains')
     color = django_filters.CharFilter(field_name='productvariant__colors__title', lookup_expr='icontains')
     material = django_filters.CharFilter(field_name='productvariant__materials__title', lookup_expr='icontains')
 
+    category_title = django_filters.CharFilter(field_name='category__title', lookup_expr='icontains')
+    category_title_uk = django_filters.CharFilter(field_name='category__title_uk', lookup_expr='icontains')
+    category_description = django_filters.CharFilter(field_name='category__description', lookup_expr='icontains')
+    category_description_uk = django_filters.CharFilter(field_name='category__description_uk', lookup_expr='icontains')
+    category_filters_title = django_filters.CharFilter(field_name='category__filters__title', lookup_expr='icontains')
+    category_filters_title_uk = django_filters.CharFilter(field_name='category__filters__title_uk', lookup_expr='icontains')
+    category_filters_description = django_filters.CharFilter(field_name='category__filters__description', lookup_expr='icontains')
+    category_filters_description_uk = django_filters.CharFilter(field_name='category__filters__description_uk', lookup_expr='icontains')
+
     class Meta:
         model = Product
-        fields = ['slug', 'title', 'category', 'price_min', 'price_max', 'size', 'color', 'material']
+        fields = ['slug', 'title', 'price_min', 'price_max', 'size', 'color', 'material', 'category_title',
+                  'category_title_uk', 'category_description', 'category_description_uk', 'category_filters_title',
+                  'category_filters_title_uk', 'category_filters_description', 'category_filters_description_uk'
+        ]
 
 
 class CategoryFilter(django_filters.FilterSet):
@@ -23,6 +35,7 @@ class CategoryFilter(django_filters.FilterSet):
     title_uk = django_filters.CharFilter(field_name='title_uk', lookup_expr='icontains')
     description = django_filters.CharFilter(field_name='description', lookup_expr='icontains')
     description_uk = django_filters.CharFilter(field_name='description_uk', lookup_expr='icontains')
+
     filters_title = django_filters.CharFilter(field_name='filters__title', lookup_expr='icontains')
     filters_title_uk = django_filters.CharFilter(field_name='filters__title_uk', lookup_expr='icontains')
     filters_description = django_filters.CharFilter(field_name='filters__description', lookup_expr='icontains')
