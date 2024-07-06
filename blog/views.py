702c8d2197881d23f86d9f53361a29989f9d53d2
hordii.rushynets@internet-techonlogy-hub.com <hordii.rushynets@internet-techonlogy-hub.com>
2024-07-06 17:43:46 +0300

    FIX set lookup_field for blog viewsets

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,17 @@
 from rest_framework import viewsets
+
 from .models import Category, Post
 from .serializers import CategorySerializer
-# Create your views here.
 
 
 class BlogCategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    lookup_field = 'slug'
 
 
 class BlogPostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = CategorySerializer
+    lookup_field = 'slug'
 
