d52fbf3d84bd7c47a9aa77a9472a2900ae7bcdd8
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-07-07 17:38:35 +0300

    CHANGED:    STATIC_URL and STATIC_ROOT

--- backend/settings.py
+++ backend/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_URL = '/statico/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/uploads/'
+STATIC_ROOT = os.path.join(BASE_DIR, "uploads")
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
