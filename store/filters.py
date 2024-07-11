d750e5e3dcc64864b6a827985bad2283506cdf10
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-07-11 16:25:57 +0300

    CORRECTION:    Multisearch    search by slug

--- store/filters.py
+++ store/filters.py
@@ -1,5 +1,16 @@
 import django_filters
 from .models import Product, Category, ProductVariant
+from django.db.models import Q
+
+
+class MultipleValuesFilter(django_filters.BaseCSVFilter, django_filters.CharFilter):
+    def filter(self, qs, value):
+        if not value:
+            return qs
+        q_objects = Q()
+        for val in value:
+            q_objects |= Q(**{f"{self.field_name}__icontains": val})
+        return qs.filter(q_objects)
 
 
 class BaseFilter(django_filters.FilterSet):
@@ -28,18 +39,19 @@ class ProductFilter(BaseFilter):
     category_description = django_filters.CharFilter(field_name='category__description', lookup_expr='icontains')
     category_description_uk = django_filters.CharFilter(field_name='category__description_uk', lookup_expr='icontains')
 
-    filter_name = django_filters.CharFilter(field_name='product_variants__filter__name', lookup_expr='icontains')
-    filter_name_uk = django_filters.CharFilter(field_name='product_variants__filter__name_uk', lookup_expr='icontains')
-    filter_fields_value = django_filters.CharFilter(field_name='product_variants__filter__filter_fields__value',
-                                                    lookup_expr='icontains')
-    filter_fields_value_uk = django_filters.CharFilter(field_name='product_variants__filter__filter_fields__value_uk',
+    filter_fields_value_en = django_filters.CharFilter(field_name='product_variants__filter_field__value_en',
                                                        lookup_expr='icontains')
+    filter_fields_value_uk = django_filters.CharFilter(field_name='product_variants__filter_field__value_uk',
+                                                       lookup_expr='icontains')
+    filter_fields_value_en_mul = MultipleValuesFilter(field_name='product_variants__filter_field__value_en')
+    filter_fields_value_uk_mul = MultipleValuesFilter(field_name='product_variants__filter_field__value_uk')
 
     class Meta:
         model = Product
-        fields = ['slug', 'title', 'description', 'price_min', 'price_max', 'size', 'color', 'material', 'category_title',
-                  'category_title_uk', 'category_description', 'category_description_uk', 'filter_name',
-                  'filter_name_uk', 'filter_fields_value', 'filter_fields_value_uk']
+        fields = ['slug', 'title', 'description', 'price_min', 'price_max', 'size', 'color', 'material',
+                  'category_title', 'category_title_uk', 'category_description', 'category_description_uk',
+                  'filter_fields_value_en', 'filter_fields_value_uk',
+                  'filter_fields_value_en_mul', 'filter_fields_value_uk_mul']
 
 
 class CategoryFilter(BaseFilter):
@@ -63,8 +75,7 @@ class ProductVariantFilter(BaseFilter):
     title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
 
     filter_fields_value = django_filters.CharFilter(field_name='filter_fields__value', lookup_expr='icontains')
-    filter_fields_value_uk = django_filters.CharFilter(field_name='filter_fields__value_uk',
-                                                           lookup_expr='icontains')
+    filter_fields_value_uk = django_filters.CharFilter(field_name='filter_fields__value_uk', lookup_expr='icontains')
 
     price_min = django_filters.NumberFilter(field_name='default_price', lookup_expr='gte')
     price_max = django_filters.NumberFilter(field_name='default_price', lookup_expr='lte')
