89b932aeec1ac661e7fc038b01135166d03e0bea
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-07-17 19:31:44 +0300

    ADDED:    Return the id of the cart item    filling the cart by field id

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,22 +1,21 @@
 from rest_framework import serializers
 from .models import Cart, CartItem
 from tep_user.serializers import UserProfileSerializer
-from store.serializers import (MaterialSerializer, SizeSerializer, FilterFieldSerializer, ColorSerializer,
-                               ProductVariantSerializer)
+from store.models import ProductVariant, Color, Material, Size, FilterField
 
 
 class CartItemSerializer(serializers.ModelSerializer):
     """CartItem Serializer"""
-    product_variants = ProductVariantSerializer()
-    color = ColorSerializer()
-    material = MaterialSerializer()
-    size = SizeSerializer()
-    filter_field = FilterFieldSerializer(many=True)
+    product_variants = serializers.PrimaryKeyRelatedField(queryset=ProductVariant.objects.all())
+    color = serializers.PrimaryKeyRelatedField(queryset=Color.objects.all())
+    material = serializers.PrimaryKeyRelatedField(queryset=Material.objects.all())
+    size = serializers.PrimaryKeyRelatedField(queryset=Size.objects.all())
+    filter_field = serializers.PrimaryKeyRelatedField(queryset=FilterField.objects.all())
     cart = serializers.PrimaryKeyRelatedField(queryset=Cart.objects.all(), required=False)
 
     class Meta:
         model = CartItem
-        fields = ['cart', 'product_variants', 'color', 'material', 'size', 'filter_field', 'quantity']
+        fields = ['id', 'cart', 'product_variants', 'color', 'material', 'size', 'filter_field', 'quantity']
 
 
 class CartSerializer(serializers.ModelSerializer):
