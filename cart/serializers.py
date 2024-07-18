1080920fa0ab7b4fd7b6a753eea4c4ee15f55ded
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-07-18 11:00:27 +0300

    FIXED:    bug with not writing in db CartItem data    bug with not returning full data CartItem

--- cart/serializers.py
+++ cart/serializers.py
@@ -2,20 +2,35 @@ from rest_framework import serializers
 from .models import Cart, CartItem
 from tep_user.serializers import UserProfileSerializer
 from store.models import ProductVariant, Color, Material, Size, FilterField
+from store.serializers import  ProductVariantSerializer, ColorSerializer, MaterialSerializer, SizeSerializer, FilterFieldSerializer
 
 
 class CartItemSerializer(serializers.ModelSerializer):
     """CartItem Serializer"""
-    product_variants = serializers.PrimaryKeyRelatedField(queryset=ProductVariant.objects.all())
-    color = serializers.PrimaryKeyRelatedField(queryset=Color.objects.all())
-    material = serializers.PrimaryKeyRelatedField(queryset=Material.objects.all())
-    size = serializers.PrimaryKeyRelatedField(queryset=Size.objects.all())
-    filter_field = serializers.PrimaryKeyRelatedField(queryset=FilterField.objects.all())
+    product_variants = ProductVariantSerializer(read_only=True)
+    product_variants_id = serializers.PrimaryKeyRelatedField(queryset=ProductVariant.objects.all(), write_only=True,
+                                                             source='product_variants')
+
+    color = ColorSerializer(read_only=True)
+    color_id = serializers.PrimaryKeyRelatedField(queryset=Color.objects.all(), write_only=True, source='color')
+
+    material = MaterialSerializer(read_only=True)
+    material_id = serializers.PrimaryKeyRelatedField(queryset=Material.objects.all(), write_only=True,
+                                                     source='material')
+
+    size = SizeSerializer(read_only=True)
+    size_id = serializers.PrimaryKeyRelatedField(queryset=Size.objects.all(), write_only=True, source='size')
+
+    filter_field = FilterFieldSerializer(read_only=True, many=True)
+    filter_field_ids = serializers.PrimaryKeyRelatedField(queryset=FilterField.objects.all(), many=True,
+                                                          write_only=True, source='filter_field')
+
     cart = serializers.PrimaryKeyRelatedField(queryset=Cart.objects.all(), required=False)
 
     class Meta:
         model = CartItem
-        fields = ['id', 'cart', 'product_variants', 'color', 'material', 'size', 'filter_field', 'quantity']
+        fields = ['id', 'cart', 'product_variants', 'product_variants_id', 'color', 'color_id', 'material',
+                  'material_id', 'size', 'size_id', 'filter_field', 'filter_field_ids', 'quantity']
 
 
 class CartSerializer(serializers.ModelSerializer):
