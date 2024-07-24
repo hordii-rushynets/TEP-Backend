3b879741a8d76951110b0f17a6a7c5f4c00625a9
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-07-24 10:51:04 +0300

    ADDED:   filters with other language

--- vacancy/filters.py
+++ vacancy/filters.py
@@ -5,17 +5,43 @@ from store.filters import MultipleStringValuesFilter, BaseFilter
 
 class VacancyFilter(BaseFilter):
     """Vacancy filter"""
-    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
-    city = django_filters.CharFilter(field_name='city', lookup_expr='icontains')
-    region = django_filters.CharFilter(field_name='region', lookup_expr='icontains')
+    title_en = django_filters.CharFilter(field_name='title_en', lookup_expr='icontains')
+    title_uk = django_filters.CharFilter(field_name='title_uk', lookup_expr='icontains')
+    title_ru = django_filters.CharFilter(field_name='title_ru', lookup_expr='icontains')
 
-    description = django_filters.CharFilter(field_name='description', lookup_expr='icontains')
-    about_company = django_filters.CharFilter(field_name='about_company', lookup_expr='icontains')
+    city_en = django_filters.CharFilter(field_name='address__city_en', lookup_expr='icontains')
+    region_en = django_filters.CharFilter(field_name='address__region_en', lookup_expr='icontains')
 
-    scope_of_work = MultipleStringValuesFilter(field_name='scope_of_work__name', lookup_expr='icontains')
-    type_of_work = MultipleStringValuesFilter(field_name='type_of_work__name', lookup_expr='icontains')
-    type_of_employment = MultipleStringValuesFilter(field_name='type_of_employment__name', lookup_expr='icontains')
-    tag = MultipleStringValuesFilter(field_name='tag__name', lookup_expr='icontains')
+    city_uk = django_filters.CharFilter(field_name='address__city_uk', lookup_expr='icontains')
+    region_uk = django_filters.CharFilter(field_name='address__region_uk', lookup_expr='icontains')
+
+    city_ru = django_filters.CharFilter(field_name='address__city_ru', lookup_expr='icontains')
+    region_ru = django_filters.CharFilter(field_name='address__region_ru', lookup_expr='icontains')
+
+    description_en = django_filters.CharFilter(field_name='description_en', lookup_expr='icontains')
+    about_company_en = django_filters.CharFilter(field_name='about_company_en', lookup_expr='icontains')
+
+    description_uk = django_filters.CharFilter(field_name='description_uk', lookup_expr='icontains')
+    about_company_uk = django_filters.CharFilter(field_name='about_company_uk', lookup_expr='icontains')
+
+    description_ru = django_filters.CharFilter(field_name='description_ru', lookup_expr='icontains')
+    about_company_ru = django_filters.CharFilter(field_name='about_company_ru', lookup_expr='icontains')
+
+    scope_of_work_en = MultipleStringValuesFilter(field_name='scope_of_work__name_en')
+    scope_of_work_uk = MultipleStringValuesFilter(field_name='scope_of_work__name_uk')
+    scope_of_work_ru = MultipleStringValuesFilter(field_name='scope_of_work__name_ru')
+
+    type_of_work_en = MultipleStringValuesFilter(field_name='type_of_work__name_en')
+    type_of_work_uk = MultipleStringValuesFilter(field_name='type_of_work__name_uk')
+    type_of_work_ru = MultipleStringValuesFilter(field_name='type_of_work__name_ru')
+
+    type_of_employment_en = MultipleStringValuesFilter(field_name='type_of_employment__name_en')
+    type_of_employment_uk = MultipleStringValuesFilter(field_name='type_of_employment__name_en')
+    type_of_employment_ry = MultipleStringValuesFilter(field_name='type_of_employment__name_en')
+
+    tag_en = MultipleStringValuesFilter(field_name='tag__name_en')
+    tag_uk = MultipleStringValuesFilter(field_name='tag__name_uk')
+    tag_ru = MultipleStringValuesFilter(field_name='tag__name_ru')
 
     class Meta:
         model = Vacancy
--- vacancy/urls.py
+++ vacancy/urls.py
@@ -1,7 +1,14 @@
-from .views import VacancyViewSet
+from .views import (VacancyViewSet, ScopeOfWorkViewSet, TypeOfWorkViewSet, TypeOfEmploymentViewSet,
+                    TagViewSet, AddressViewSet)
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'', VacancyViewSet, basename='vacancy')
+router.register(r'scope-of-work', ScopeOfWorkViewSet, basename='scope_of_work')
+router.register(r'type-of-work', TypeOfWorkViewSet, basename='type_of_work')
+router.register(r'type-of-employment', TypeOfEmploymentViewSet, basename='type_of_employment')
+router.register(r'tag', TagViewSet, basename='tag')
+router.register(r'address', AddressViewSet, basename='address')
+
 
 urlpatterns = router.urls + []
