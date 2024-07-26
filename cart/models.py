789d859ec35f5afd38e02ed6a83a494d51cb0c4a
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-07-26 22:29:21 +0300

    FIXED:   Add different elements with the same color, size, and material

--- cart/models.py
+++ cart/models.py
@@ -16,9 +16,9 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='order')
     product_variants = models.OneToOneField(ProductVariant, on_delete=models.CASCADE, related_name='cart_item',
                                             blank=True)
-    color = models.OneToOneField(Color, on_delete=models.SET_NULL, related_name='colour', blank=True, null=True)
-    material = models.OneToOneField(Material, on_delete=models.SET_NULL, related_name='material', blank=True, null=True)
-    size = models.OneToOneField(Size, on_delete=models.SET_NULL, related_name='size', blank=True, null=True)
+    color = models.ForeignKey(Color, on_delete=models.SET_NULL, related_name='colour', blank=True, null=True)
+    material = models.ForeignKey(Material, on_delete=models.SET_NULL, related_name='material', blank=True, null=True)
+    size = models.ForeignKey(Size, on_delete=models.SET_NULL, related_name='size', blank=True, null=True)
     filter_field = models.ManyToManyField(FilterField, related_name='cart_items', blank=True)
     quantity = models.PositiveIntegerField(default=1)
 
