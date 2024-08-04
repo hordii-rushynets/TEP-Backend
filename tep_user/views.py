97861a6d7afd03a19690a1f5ae43f9e3f8f360ce
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-08-05 00:54:22 +0300

    ADDED:   added save first and last name

--- tep_user/views.py
+++ tep_user/views.py
@@ -118,13 +118,22 @@ class UserEmailUpdateViewSet(viewsets.GenericViewSet):
 class GoogleLoginView(APIView):
     def post(self, request):
         token = request.data.get('token')
+
         try:
-            id_info = id_token.verify_oauth2_token(token, requests.Request(), settings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY)
+            id_info = id_token.verify_oauth2_token(token, requests.Request())
 
             if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
                 raise ValueError('Wrong issuer.')
 
-            user, _ = TEPUser.objects.get_or_create(email=id_info['email'], defaults={'username': id_info['email']})
+            user, created = TEPUser.objects.get_or_create(
+                email=id_info['email'],
+                defaults={
+                    'username': id_info['email'],
+                    'first_name': id_info.get('given_name', ''),
+                    'last_name': id_info.get('family_name', ''),
+                }
+            )
+
             refresh = RefreshToken.for_user(user)
             data = {
                 'email': user.email,
@@ -132,6 +141,7 @@ class GoogleLoginView(APIView):
                 'refresh': str(refresh)
             }
             return Response(data, status=status.HTTP_200_OK)
-
-        except ValueError:
-            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+        except ValueError as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': 'Something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
