f2ba737e945bee1c95947ae985543a5119daa87c
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-08-20 12:02:00 +0300

    Fixed:    Remove fields in the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -8,10 +8,14 @@ admin.site.register(Tag)
 class SectionInline(admin.TabularInline):
     model = Section
     extra = 1
-    fields = ('types', 'title_uk', 'title_en', 'description_uk', 'description_en', 'additional_description_uk', 'additional_description_en', 'image')
+    fields = ('types', 'title_uk', 'title_en', 'title_ru', 'description_uk', 'description_en', 'description_ru',
+              'additional_description_uk', 'additional_description_en', 'additional_description_ru', 'image')
+    exclude = ('title', 'description', 'additional_description')
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    fields = [('title_uk', 'title_en', 'slug'), 'tags', 'author', 'meta_description_uk', 'meta_description_en', 'image']
+    fields = ['title_uk', 'title_en', 'title_ru', 'slug', 'tags', 'author',
+              'meta_description_uk', 'meta_description_en', 'meta_description_ru', 'image']
     inlines = [SectionInline]
+    exclude = ('title', 'meta_description')
