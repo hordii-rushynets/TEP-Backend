7429cc6228335054b29a5a7bcab2a92835484c83
hordii.rushynets@internet-techonlogy-hub.com <hordii.rushynets@internet-techonlogy-hub.com>
2024-09-15 06:42:47 +0300

    ADD types annotations

--- post/services/factory.py
+++ post/services/factory.py
@@ -1,10 +1,11 @@
 from rest_framework.exceptions import ValidationError
 
+from .abstract_delivery_service import AbstractDeliveryService
 from .nova_poshta_delivery_service import NovaPoshtaService
 from .ukr_poshta_delivery_service import UkrPoshtaDeliveryService
 
 
-def get_delivery_service(service_type):
+def get_delivery_service(service_type: str) -> AbstractDeliveryService:
     if service_type == 'NovaPost':
         return NovaPoshtaService()
     elif service_type == 'UkrPost':
