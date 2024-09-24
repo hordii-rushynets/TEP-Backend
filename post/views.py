ff083dc08da697897c7fc7e84cb15c89ee9ac37c
hordii.rushynets@internet-techonlogy-hub.com <hordii.rushynets@internet-techonlogy-hub.com>
2024-09-24 11:57:41 +0300

    Code improvement

--- post/views.py
+++ post/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.db.models import Q
 from django.shortcuts import render
 from rest_framework import status, viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -167,12 +168,7 @@ class OrderViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        if user.is_authenticated:
-            order = Order.objects.filter(tep_user=user)
-        else:
-            ip_service = IPControlService(self.request, RedisDatabases.IP_CONTROL)
-            ip_address = ip_service.get_ip()
-            order = Order.objects.filter(ip_address=ip_address)
-
-        return order
+        ip_address = IPControlService(self.request, RedisDatabases.IP_CONTROL).get_ip()
+        query = Q(tep_user=user) if user.is_authenticated else Q(ip_address=ip_address)
+        return Order.objects.filter(query)
 
