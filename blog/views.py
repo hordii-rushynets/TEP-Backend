69f557008ea06bd78dfefca236193dc138773555
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-10-23 20:54:24 +0300

    ADDED: cache in blog

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,21 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
 from rest_framework import viewsets
+
 from .pagination import PostPagination
 from .models import Tag, Post
 from .serializers import TagSerializer, PostSerializer
 
 
+@method_decorator(cache_page(60 * 60 * 3), name='dispatch')
 class BlogCategoryViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     lookup_field = 'slug'
 
 
+@method_decorator(cache_page(60 * 60 * 3), name='dispatch')
 class BlogPostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
